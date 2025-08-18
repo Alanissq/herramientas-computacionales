d443c4b0384be5a5ad3fc9363a74746e6b06d7c5
Alanis soto quezada <alanis.soto@ug.uchile.cl>
2025-08-18 15:34:56 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight)  
-  background(33,52,73)
+  createCanvas(windowWidth, windowHeight);
+  background(33, 52, 73);
 }
 
 function draw() {
- d=random(0,255)
- fill(random(0,255),random(0,255),random(0,255))
-  ellipse(mouseX, mouseY,d,d)
-
+  d = random(0, 60);
+  colorMode(HSB, 360.1, 100);
+  fill(random(0, 359), 100, 100);
+  ellipse(mouseX, mouseY, d, d);
 }
 function keyPressed() {
-  if (key === 'p') {
-    save('dibujo.jpg');
+  if (key === "p") {
+    save("dibujo.jpg");
   }
 }
